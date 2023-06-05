b8cfae1eba738ac892d2b3e61564c15b2850c088
Duco Telkamp <duco@debian-BULLSEYE-live-builder-AMD64>
2023-06-05 16:27:03 +0200

    Started on initialiser Game

--- Scripts/game.py
+++ Scripts/game.py
@@ -1,2 +1,11 @@
+from car import Car
+
 class Game:
-    pass
+    
+    def __init__(self, file_name: str) -> None:
+    	self.cars: list[Car]= []
+    	self.load_cars(file_name)
+    	
+    def load_cars(file_name: str) -> None:
+    	# Append self.cars with all cars in filename csv
+    	pass
