730a61af537f46dbad2775376de29fefe3fdb7ef
Duco Telkamp <duco@debian-BULLSEYE-live-builder-AMD64>
2023-06-06 10:14:18 +0200

    Removed load_cars to prevent merge conflicts

--- Scripts/game.py
+++ Scripts/game.py
@@ -4,8 +4,3 @@ class Game:
     
     def __init__(self, file_name: str) -> None:
     	self.cars: dict[str, Car]= {}
-    	self.load_cars(file_name)
-    	
-    def load_cars(file_name: str) -> None:
-    	# Append self.cars with all cars in filename csv
-    	pass
