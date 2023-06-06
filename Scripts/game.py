22ef89c9937d6f95524a0e33ab4a94d76555090b
Sabrina Strijker <sabrinastrijker@gmail.com>
2023-06-06 11:11:14 +0200

    Added str function to game.py

--- Scripts/game.py
+++ Scripts/game.py
@@ -21,15 +21,23 @@ class Game:
                 self.cars[car] = Car(orientation, int(col), int(row), int(length))
 
 
-    def is_won(self) -> bool:
+    def is_won(self) -> bool: 
         return self.cars['X'].get_col() == self.dimension - 1
 
 
+    def __str__(self) -> str:
+        board_string = ''
+        for _ in range(self.dimension):
+            board_string += ' _ ' * self.dimension + '\n'
+
+        return board_string 
 
 if __name__ == '__main__': 
     g = Game('/home/sabrinastrijker/AH/AH-RushHour-DJS/Input/Rushhour6x6_1.csv', 6)
     print(g.cars['X'])
 
     print(g.is_won())
+
+    print(g.__str__())
 
 
