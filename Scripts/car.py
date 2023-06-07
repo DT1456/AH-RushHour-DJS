fd1d6b0810db407367f51bf86e5055835aaf4b10
Duco Telkamp <duco@debian-BULLSEYE-live-builder-AMD64>
2023-06-07 11:10:36 +0200

    Minor style polish (flake8) car.py

--- Scripts/car.py
+++ Scripts/car.py
@@ -6,11 +6,11 @@ class Car:
         self.col = col
         self.row = row
         self.length = length
-        
+
     def get_col(self) -> int:
         """Returns column of Car"""
         return self.col
-    
+
     def get_row(self) -> int:
         """Returns row of Car"""
         return self.row
@@ -22,4 +22,3 @@ class Car:
     def set_row(self, row: int) -> None:
         """Changes row of Car"""
         self.row = row
-    
