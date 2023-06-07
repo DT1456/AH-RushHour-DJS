d6b4f68fcd8613ada2a6f9cc86cee0029aa0306d
JasmijnSmidt <jasmijnsmidt@gmail.com>
2023-06-07 10:36:01 +0200

    added get col/row and set col/row functions to Car

--- Scripts/car.py
+++ Scripts/car.py
@@ -1,10 +1,25 @@
 class Car:
 
     def __init__(self, orientation: str, col: int, row: int, length: int):
+        """Initializes Car useing column, row and length"""
         self.orientation = orientation
         self.col = col
         self.row = row
         self.length = length
         
     def get_col(self) -> int:
-    	return self.col
+        """Returns column of Car"""
+        return self.col
+    
+    def get_row(self) -> int:
+        """Returns row of Car"""
+        return self.row
+
+    def set_col(self, col: int) -> None:
+        """Changes column of Car"""
+        self.col = col
+
+    def set_row(self, row: int) -> None:
+        """Changes row of Car"""
+        self.row = row
+    
