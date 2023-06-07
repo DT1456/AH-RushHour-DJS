f8a57585f201964c0d8e0ceaae517122dfc37a93
Sabrina Strijker <sabrinastrijker@gmail.com>
2023-06-07 14:16:56 +0200

    Cleaned up the code and rewrote in functions

--- Scripts/board_visual.py
+++ Scripts/board_visual.py
@@ -1,3 +1,4 @@
+
 import random
 import string
 from collections import defaultdict
@@ -13,11 +14,39 @@ def generate_random_color():
 
 def colorize_string_list(string_list):
     """
-    colour each char in the string list with a random color.
+    Colour each character in the string list with a random color.
     """
     color_map = defaultdict(generate_random_color)
     colorized_string_list = [color_map[char] for char in string_list]
     return colorized_string_list
+
+def convert_color_strings_to_rgb(color_strings):
+    """
+    Convert color strings to RGB values.
+    """
+    rgb_values = [mcolors.hex2color(color) for color in color_strings]
+    print(rgb_values)
+    return rgb_values
+
+def create_color_matrix(data):
+    """
+    Create an empty matrix to hold the color.
+    """
+    x_coords = []
+    y_coords = []
+    colors = []
+
+    for coord, color in data.items():
+        y_coords.append(coord[0])
+        x_coords.append(coord[1])
+        colors.append(color)
+
+    colored_string_list = colorize_string_list(colors)
+    rgb_values = convert_color_strings_to_rgb(colored_string_list)
+
+    color_matrix = np.empty((len(y_coords), len(x_coords), 3), dtype='float')
+    
+    return color_matrix
 
 data = {(1, 1): '_', (1, 2): 'A', (1, 3): 'A', (1, 4): 'B', (1, 5): 'B', (1, 6): 'B',
         (2, 1): '_', (2, 2): 'C', (2, 3): 'C', (2, 4): 'E', (2, 5): 'D', (2, 6): 'D',
@@ -26,27 +55,7 @@ data = {(1, 1): '_', (1, 2): 'A', (1, 3): 'A', (1, 4): 'B', (1, 5): 'B', (1, 6):
         (5, 1): 'K', (5, 2): '_', (5, 3): 'L', (5, 4): '_', (5, 5): 'J', (5, 6): 'J',
         (6, 1): 'K', (6, 2): '_', (6, 3): 'L', (6, 4): '_', (6, 5): '_', (6, 6): '_'}
 
-x_coords = []
-y_coords = []
-colors = [] 
-
-for coord, color in data.items():
-    y_coords.append(coord[0])
-    x_coords.append(coord[1])
-    colors.append(color)
-
-colored_string_list = colorize_string_list(colors)
-
-# Convert color strings to RGB values
-rgb_values = [mcolors.hex2color(color) for color in colored_string_list]
-
-# Print to check if the colours work
-print(rgb_values)
-
-
-
-# Create an empty matrix to hold the color
-color_matrix = np.empty((len(y_coords), len(x_coords), 3), dtype='float')
+color_matrix = create_color_matrix(data)
 
 
 
