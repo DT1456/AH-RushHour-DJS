7fedd4c9e09e37dc23156de864e00fa8baceb763
JasmijnSmidt <jasmijnsmidt@gmail.com>
2023-06-13 11:59:23 +0200

    improved breadth_solver.py

--- Scripts/breadth_solver.py
+++ Scripts/breadth_solver.py
@@ -20,16 +20,16 @@ class Solver:
         return reverse_direction
 
     def fill_states(self, game: Game) -> None:
-        self.states[game.__str__] = ()
-        old_game_str = game.__str__
+        self.states[game.__str__()] = []
+        old_game_str = game.__str__()
 
         moves_list = self.get_possible_moves(game)
         for move in moves_list:
             game.move(move[0], move[1])
-            if game.__str__ not in self.states:
-                self.states[game.__str__] = move
-            elif len(self.states[game.__str__]) > len(self.states[old_game_str] + move):
-                self.states[game.__str__] = self.states[old_game_str] + move
+            if game.__str__() not in self.states:
+                self.states[game.__str__()] = [move]
+            elif len(self.states[game.__str__()]) > len(self.states[old_game_str] + [move]):
+                self.states[game.__str__()] = self.states[old_game_str] + [move]
 
             reverse_direction = self.reverse_direction(move[1])
             game.move(move[0], reverse_direction)
@@ -39,5 +39,16 @@ class Solver:
         for car_name, car in zip(game.cars, game.cars.values()):
             for direction in ['L', 'R', 'U', 'D']:
                 if game.is_valid_move(car_name, direction):
-                    moves_list.append(car_name, direction)
+                    moves_list.append((car_name, direction))
         return moves_list
+
+    def play_move(self, game: Game) -> Game:
+        if len(self.states) == 0:
+            self.fill_states(game)
+        print('-----------------------------')
+        for state in self.states:
+            print(str(state))
+            print(self.states[state])
+            print(len(self.states[state]))
+            print('-----------------------------')
+        print(1/0)
