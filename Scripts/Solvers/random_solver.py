8a67b036367e60880900f03351527edf395bcff9
Duco Telkamp <duco@debian-BULLSEYE-live-builder-AMD64>
2023-06-15 17:02:41 +0200

    Changed random solver, added new random solver with heuristics

--- Scripts/Solvers/random_solver.py
+++ Scripts/Solvers/random_solver.py
@@ -5,18 +5,23 @@ import random as random
 class Solver:
 
     def __init__(self) -> None:
-        """Initialise the possible directions to be chosen in every move"""
-        self.possible_directions: list[str] = ['L', 'R', 'U', 'D']
+        pass
 
     def play_move(self, game: Game) -> Game:
         """Play one move in order to solve the game"""
-        while True:
-            # Pick a random car and a random direction
-            car_name = random.SystemRandom().choice(list(game.cars))
-            direction = random.SystemRandom().choice(self.possible_directions)
-
-            # If the chosen car and direction represent a valid move, move
-            if game.move(car_name, direction):
-                break
+        moves_list = self.get_possible_moves(game)
+        # Pick a random car and a random direction
+        move = random.SystemRandom().choice(moves_list)
+        car_name, direction = move
+        game.move(car_name, direction)
 
         return game
+
+    def get_possible_moves(self, game: Game) -> list[tuple[str, str]]:
+        """Get list of possible moves in this state"""
+        moves_list = []
+        for car_name, car in zip(game.cars, game.cars.values()):
+            for direction in ['L', 'R', 'U', 'D']:
+                if game.is_valid_move(car_name, direction):
+                    moves_list.append((car_name, direction))
+        return moves_list
