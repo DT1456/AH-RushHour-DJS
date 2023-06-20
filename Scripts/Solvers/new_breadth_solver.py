2e28a7a2f9c12df88213aabe66e8a07736cec155
JasmijnSmidt <jasmijnsmidt@gmail.com>
2023-06-20 09:55:16 +0200

    added reverse_direction and get_possible_moves to new breadth solver implementation

--- Scripts/Solvers/new_breadth_solver.py
+++ Scripts/Solvers/new_breadth_solver.py
@@ -1,3 +1,5 @@
+from game import Game
+
 class Queue:
 
     # Initialiase empty queue
@@ -16,3 +18,31 @@ class Queue:
     # find and return size of the queue
     def size(self):
         return len(self._data)
+
+class Solver:
+    def __init__(self):
+        self.parents: dict[str, str]
+        self.queue = Queue()
+        self.original_board: str
+        self.parents_move: dict[str, tuple[str, str]]
+
+    def get_possible_moves(self, game: Game) -> list[tuple[str, str]]:
+        """Get list of possible moves in this state"""
+        moves_list = []
+        for car_name, car in zip(game.cars, game.cars.values()):
+            for direction in ['L', 'R', 'U', 'D']:
+                if game.is_valid_move(car_name, direction):
+                    moves_list.append((car_name, direction))
+        return moves_list
+        
+    def reverse_direction(self, direction: str) -> str:
+        """Defining and returning a reversed direction"""
+        reverse_direction = 'L'
+        if direction == 'L':
+            reverse_direction = 'R'
+        elif direction == 'U':
+            reverse_direction = 'D'
+        elif direction == 'D':
+            reverse_direction = 'U'
+
+        return reverse_direction
