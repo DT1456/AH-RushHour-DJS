5a0203f6b8cde6edf0f3fb7eb0e37ddce46e982c
JasmijnSmidt <jasmijnsmidt@gmail.com>
2023-06-20 10:38:21 +0200

    added move_to_state method to solver

--- Scripts/Solvers/new_breadth_solver.py
+++ Scripts/Solvers/new_breadth_solver.py
@@ -2,7 +2,7 @@ from game import Game
 
 class Queue:
 
-    # Initialiase empty queue
+    # Initialise empty queue
     def __init__(self):
         self._data = []
 
@@ -15,15 +15,17 @@ class Queue:
         assert self.size() > 0
         return self._data.pop(0)
 
-    # find and return size of the queue
+    # Find and return size of the queue
     def size(self):
         return len(self._data)
 
 class Solver:
     def __init__(self):
+        # dict of states previous to current state (key) and current states
         self.parents: dict[str, str]
         self.queue = Queue()
         self.original_board: str
+        # dict of moves that that take you from previous state to the current state
         self.parents_move: dict[str, tuple[str, str]]
 
     def get_possible_moves(self, game: Game) -> list[tuple[str, str]]:
@@ -46,3 +48,27 @@ class Solver:
             reverse_direction = 'U'
 
         return reverse_direction
+    
+    def move_to_state(self, game: Game, state: str) -> Game:
+        # Moving backwards
+        while str(game) != self.original_board:
+            move = self.parents_move[str(game)]
+            car_name, direction = move
+            game.move(car_name, self.reverse_direction(direction))
+        
+        # Fill forward moves
+        moves_forward = []
+
+        while self.original_board != state:
+            moves_forward.append(self.parents_move[state])
+            state = self.parents[state]
+        
+        # Move forwards
+
+        # Reverse list put most recent move to front
+        moves_forward.reverse()
+        for move in moves_forward:
+            car_name, direction = move
+            game.move(car_name, direction)
+        
+        return game
